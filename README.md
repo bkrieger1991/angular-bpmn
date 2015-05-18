a3cae3f306b009cfcf4ebfb763a437784967ef93
Benjamin Krieger <flux851991@web.de>
2015-05-18 20:24:15 +0200

    Initial commit, project setup

---
+++ README.md
@@ -0,0 +1,10 @@
+#AngularJS based editor to create bpmn diagrams (and read out all the process-information)
+
+## Getting Started
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
---
+++ src/module.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @namespace angular-bpmn
+ */
+
+angular.module('angular-bpmn', [])
+.provider('$ngBpmn', function() {
+
+var defaults = this.defaults = {};
+
+this.$get = function() {};
+
+});
